31dd70681a28bc26d025026125cb849117629ff3
Wangmx <15825529523@163.com>
2018-03-13 22:39:28 +0800

    添加video发送线程。

---
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/VedioSenderThread.java
@@ -0,0 +1,15 @@
+package com.blink.live.blinkstreamlib.core;
+
+import android.media.MediaCodec;
+
+/**
+ * <pre>
+ *     author : wangmingxing
+ *     time   : 2018/3/13
+ *     desc   : vedio 发送线程
+ * </pre>
+ */
+public class VedioSenderThread {
+    private static final long WAIT_TIME = 5000;
+    private MediaCodec.BufferInfo eInfo;
+}
