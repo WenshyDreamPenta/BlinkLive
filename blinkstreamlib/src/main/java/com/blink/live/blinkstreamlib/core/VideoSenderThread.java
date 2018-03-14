a0f6f6ed599d9be0a471f792ea63a5a7a863f00a
Wangmx <15825529523@163.com>
2018-03-14 21:58:54 +0800

    update VideoSenderThread

--- blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/VideoSenderThread.java
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/VideoSenderThread.java
@@ -1,8 +1,12 @@
 package com.blink.live.blinkstreamlib.core;
 
 import android.media.MediaCodec;
+import android.media.MediaFormat;
 
 import com.blink.live.blinkstreamlib.rtmp.RESFlvDataCollecter;
+import com.blink.live.blinkstreamlib.utils.LogTools;
+
+import java.nio.ByteBuffer;
 
 /**
  * <pre>
@@ -11,7 +15,7 @@ import com.blink.live.blinkstreamlib.rtmp.RESFlvDataCollecter;
  *     desc   : vedio 发送线程
  * </pre>
  */
-public class VideoSenderThread extends Thread{
+public class VideoSenderThread extends Thread {
     private static final long WAIT_TIME = 5000;
     private MediaCodec.BufferInfo eInfo;
     private long startTime = 0;
@@ -29,20 +33,74 @@ public class VideoSenderThread extends Thread{
     }
 
     //更新编码器
-    public void updateMediaCodec(MediaCodec encodec){
-        synchronized (syncDstVideoEncoder){
-            dstVideoEncoder =encodec;
+    public void updateMediaCodec(MediaCodec encodec) {
+        synchronized (syncDstVideoEncoder) {
+            dstVideoEncoder = encodec;
         }
     }
 
     //退出线程
-    void quit(){
+    void quit() {
         shouldQuit = true;
         this.interrupt();
     }
 
     @Override
     public void run() {
+        while (!shouldQuit) {
+            synchronized (syncDstVideoEncoder) {
+                int eobIndex = MediaCodec.INFO_TRY_AGAIN_LATER;
+                try {
+                    eobIndex = dstVideoEncoder.dequeueOutputBuffer(eInfo, WAIT_TIME);
+                }
+                catch (Exception exception) {
+                }
+                switch (eobIndex) {
+                    case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
+                        LogTools.d("VideoSenderThread,MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED");
+                        break;
+                    case MediaCodec.INFO_TRY_AGAIN_LATER:
+                        LogTools.d("VideoSenderThread,MediaCodec.INFO_TRY_AGAIN_LATER");
+                        break;
+                    case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+                        LogTools.d("VideoSenderThread,MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:" +
+                                dstVideoEncoder.getOutputFormat().toString());
+                        sendAVCDecoderConfigurationRecord(0, dstVideoEncoder.getOutputFormat());
+                        break;
+                    default:
+                        LogTools.d("VideoSenderThread,MediaCode,eobIndex=" + eobIndex);
+                        if (startTime == 0) {
+                            startTime = eInfo.presentationTimeUs / 1000;
+                        }
+                        /**
+                         * we send sps pps already in INFO_OUTPUT_FORMAT_CHANGED
+                         * so we ignore MediaCodec.BUFFER_FLAG_CODEC_CONFIG
+                         */
+                        if (eInfo.flags != MediaCodec.BUFFER_FLAG_CODEC_CONFIG && eInfo.size != 0) {
+                            ByteBuffer realData = dstVideoEncoder.getOutputBuffers()[eobIndex];
+                            realData.position(eInfo.offset + 4);
+                            realData.limit(eInfo.offset + eInfo.size);
+                            sendRealData((eInfo.presentationTimeUs / 1000) - startTime, realData);
+                        }
+                        dstVideoEncoder.releaseOutputBuffer(eobIndex, false);
+                        break;
+                }
+
+            }
+            try {
+                sleep(5);
+            }
+            catch (Exception e) {
+            }
+        }
+        eInfo = null;
+    }
+
+    private void sendAVCDecoderConfigurationRecord(long tms, MediaFormat format) {
+
+    }
+
+    private void sendRealData(long tms, ByteBuffer realData) {
 
     }
 }
