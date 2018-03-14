15fe382a09c44326e836305ca13ea72d3a41bf2f
Wangmx <15825529523@163.com>
2018-03-14 23:03:42 +0800

    h264 flv编码配置类

---
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/utils/ByteArrayTools.java
@@ -0,0 +1,22 @@
+package com.blink.live.blinkstreamlib.utils;
+
+/**
+ * <pre>
+ *     author : wangmingxing
+ *     time   : 2018/3/14
+ *     desc   : Byte Array Tools
+ * </pre>
+ */
+public class ByteArrayTools {
+    public static void intToByteArrayFull(byte[] dst, int pos, int interger) {
+        dst[pos] = (byte) ((interger >> 24) & 0xFF);
+        dst[pos + 1] = (byte) ((interger >> 16) & 0xFF);
+        dst[pos + 2] = (byte) ((interger >> 8) & 0xFF);
+        dst[pos + 3] = (byte) ((interger) & 0xFF);
+    }
+
+    public static void intToByteArrayTwoByte(byte[] dst, int pos, int interger) {
+        dst[pos] = (byte) ((interger >> 8) & 0xFF);
+        dst[pos + 1] = (byte) ((interger) & 0xFF);
+    }
+}
