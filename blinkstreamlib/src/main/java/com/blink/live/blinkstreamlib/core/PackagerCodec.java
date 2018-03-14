1fc6141b9ea6948c81e977dc956ff1dc64bbe124
Wangmx <15825529523@163.com>
2018-03-14 22:05:46 +0800

    添加h264 flv编码类

---
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/PackagerCodec.java
@@ -0,0 +1,12 @@
+package com.blink.live.blinkstreamlib.core;
+
+/**
+ * <pre>
+ *     author : wangmingxing
+ *     time   : 2018/3/14
+ *     desc   : H264编码  FLVPackager
+ * </pre>
+ */
+public class PackagerCodec {
+    
+}
