f09cbe60830427295fa2144fb343fd55f8c1daa1
Wangmx <15825529523@163.com>
2018-03-15 22:56:48 +0800

    audio 发送线程

---
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/AudioSenderThread.java
@@ -0,0 +1,12 @@
+package com.blink.live.blinkstreamlib.core;
+
+/**
+ * <pre>
+ *     author : wangmingxing
+ *     time   : 2018/3/15
+ *     desc   : audio 发送线程
+ * </pre>
+ */
+public class AudioSenderThread extends Thread {
+
+}
