b9c1da1e8f91dfb1836ef368a51fc2822a441ef0
Wangmx <15825529523@163.com>
2018-03-15 22:37:36 +0800

    添加音频软编码

--- blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/MediaCodecHelper.java
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/MediaCodecHelper.java
@@ -17,12 +17,13 @@ import com.blink.live.blinkstreamlib.utils.LogTools;
 public class MediaCodecHelper {
 
     /**
-     *       创建video 软编码
-     * @param coreParameters
-     * @param videoFormat
-     * @return
+     * 创建video软编码
+     *
+     * @param coreParameters 编码参数
+     * @param videoFormat    视频格式
+     * @return MediaCodec对象
      */
-    public static MediaCodec createSoftVideoMediaCodec(RESCoreParameters coreParameters, MediaFormat videoFormat){
+    public static MediaCodec createSoftVideoMediaCodec(RESCoreParameters coreParameters, MediaFormat videoFormat) {
         videoFormat.setString(MediaFormat.KEY_MIME, "video/avc");
         videoFormat.setInteger(MediaFormat.KEY_WIDTH, coreParameters.videoWidth);
         videoFormat.setInteger(MediaFormat.KEY_HEIGHT, coreParameters.videoHeight);
@@ -33,31 +34,60 @@ public class MediaCodecHelper {
         videoFormat.setInteger(MediaFormat.KEY_LEVEL, MediaCodecInfo.CodecProfileLevel.AVCLevel31);
         videoFormat.setInteger(MediaFormat.KEY_BITRATE_MODE, MediaCodecInfo.EncoderCapabilities.BITRATE_MODE_CBR);
         MediaCodec result = null;
-        try{
+        try {
             result = MediaCodec.createEncoderByType(videoFormat.getString(MediaFormat.KEY_MIME));
-            int[] colorful = result.getCodecInfo().getCapabilitiesForType(videoFormat.getString(MediaFormat.KEY_MIME)).colorFormats;
+            int[] colorful = result.getCodecInfo()
+                    .getCapabilitiesForType(videoFormat.getString(MediaFormat.KEY_MIME)).colorFormats;
             int dstVideoColorFormat = -1;
             //select mediacodec colorformat
             if (isArrayContain(colorful, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar)) {
                 dstVideoColorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar;
                 coreParameters.mediacodecAVCColorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar;
             }
-            if (dstVideoColorFormat == -1 && isArrayContain(colorful, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar)) {
+            if (dstVideoColorFormat == -1 &&
+                    isArrayContain(colorful, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar)) {
                 dstVideoColorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar;
                 coreParameters.mediacodecAVCColorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar;
             }
             if (dstVideoColorFormat == -1) {
-                LogTools.e("!!!!!!!!!!!UnSupport,mediaCodecColorFormat");
+                LogTools.e("UnSupport,mediaCodecColorFormat");
                 return null;
             }
             videoFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, dstVideoColorFormat);
-        }catch (Exception e){
+        }
+        catch (Exception e) {
             LogTools.trace(e);
             return null;
         }
-
         return result;
 
+    }
+
+    /**
+     * 创建Audio软编码
+     *
+     * @param coreParameters 编码参数
+     * @param audioFormat    音频格式
+     * @return MediaCodec对象
+     */
+    public static MediaCodec createAudioMediaCodec(RESCoreParameters coreParameters, MediaFormat audioFormat) {
+        //Audio
+        MediaCodec result;
+        audioFormat.setString(MediaFormat.KEY_MIME, "audio/mp4a-latm");
+        audioFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, coreParameters.mediacodecAACProfile);
+        audioFormat.setInteger(MediaFormat.KEY_SAMPLE_RATE, coreParameters.mediacodecAACSampleRate);
+        audioFormat.setInteger(MediaFormat.KEY_CHANNEL_COUNT, coreParameters.mediacodecAACChannelCount);
+        audioFormat.setInteger(MediaFormat.KEY_BIT_RATE, coreParameters.mediacodecAACBitRate);
+        audioFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, coreParameters.mediacodecAACMaxInputSize);
+        LogTools.d("creatingAudioEncoder,format=" + audioFormat.toString());
+        try {
+            result = MediaCodec.createEncoderByType(audioFormat.getString(MediaFormat.KEY_MIME));
+        }
+        catch (Exception e) {
+            LogTools.trace("can`t create audioEncoder!", e);
+            return null;
+        }
+        return result;
     }
 
     private static boolean isArrayContain(int[] src, int target) {
@@ -68,4 +98,5 @@ public class MediaCodecHelper {
         }
         return false;
     }
+
 }
