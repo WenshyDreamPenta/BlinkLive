7ee0ca1576311ccb47b852ae202798ad07c03fb8
Wangmx <15825529523@163.com>
2018-03-16 16:25:00 +0800

    添加AudioSenderThread

--- blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/AudioSenderThread.java
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/core/AudioSenderThread.java
@@ -1,5 +1,9 @@
 package com.blink.live.blinkstreamlib.core;
 
+import android.media.MediaCodec;
+
+import com.blink.live.blinkstreamlib.rtmp.RESFlvDataCollecter;
+
 /**
  * <pre>
  *     author : wangmingxing
@@ -8,5 +12,26 @@ package com.blink.live.blinkstreamlib.core;
  * </pre>
  */
 public class AudioSenderThread extends Thread {
+    //ms
+    private static final long WAIT_TIME = 5000;
+    private MediaCodec.BufferInfo mBufferInfo;
+    private long startTime = 0;
+    private MediaCodec audioEncoder;
+    private RESFlvDataCollecter dataCollecter;
 
+    private boolean shouldQuit = false;
+
+    AudioSenderThread(String name, MediaCodec encoder, RESFlvDataCollecter flvDataCollecter){
+        super(name);
+        mBufferInfo = new MediaCodec.BufferInfo();
+        startTime = 0;
+        audioEncoder = encoder;
+        dataCollecter = flvDataCollecter;
+    }
+
+    //中断线程
+    void quit(){
+        shouldQuit = true;
+        this.interrupt();
+    }
 }
