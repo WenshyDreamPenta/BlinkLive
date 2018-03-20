41739b6dbfd8a67e13ea7a3f0a331e53eaf1367e
Wangmingxing <starpentadream@gmail.com>
2018-03-20 14:52:50 +0800

    update gradle

--- app/src/main/java/com/blink/live/LiveActivity.java
+++ app/src/main/java/com/blink/live/LiveActivity.java
@@ -1,13 +1,56 @@
 package com.blink.live;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class LiveActivity extends AppCompatActivity {
+import com.blink.framelibrary.base.activity.BaseEventActivity;
+import com.blink.framelibrary.eventbus.EventMap;
+
+public class LiveActivity extends BaseEventActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live);
+    }
+
+    @Override
+    protected void handleEvent(EventMap.BaseEvent event) {
+
+    }
+
+    @Override
+    protected boolean isEventActivity() {
+        return false;
+    }
+
+    @Override
+    public void initData(Bundle bundle) {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void onWidgetClick(View view) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void initEvents() {
+
+    }
+
+    @Override
+    public void initViews() {
+
     }
 }
