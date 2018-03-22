516f52dde28246ade05d76c015f61e7d3138c278
Wangmingxing <starpentadream@gmail.com>
2018-03-22 23:35:54 +0800

    add BaseSoftVideoFilter

---
+++ blinkstreamlib/src/main/java/com/blink/live/blinkstreamlib/filter/BaseSoftVideoFilter.java
@@ -0,0 +1,11 @@
+package com.blink.live.blinkstreamlib.filter;
+
+/**
+ * <pre>
+ *     author : wangmingxing
+ *     time   : 2018/3/22
+ *     desc   : 视频软编码
+ * </pre>
+ */
+public class BaseSoftVideoFilter {
+}
